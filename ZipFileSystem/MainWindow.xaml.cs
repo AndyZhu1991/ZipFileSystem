2579eafeb8f516170f5e09e890d69e5056bb6701
zhujinchang <stdzhu@gmail.com>
2020-11-02 18:48:55 +0800

    Add some basic componts to allow us implement DokanOperations.

--- ZipFileSystem/MainWindow.xaml.cs
+++ ZipFileSystem/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using DokanNet;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZipFileSystem.Dokans;
+using ZipFileSystem.ZipTree;
 
 namespace ZipFileSystem
 {
@@ -20,9 +26,66 @@ namespace ZipFileSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static char MOUNT_POINT = 'z';
+
+        private FileTreeNode mRoot;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenZipFile();
+            Dokan.Unmount(MOUNT_POINT);
+        }
+
+        private async void OpenZipFile()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                DefaultExt = ".zip", // Default file extension
+                Filter = "Compressed file (.zip)|*.zip" // Filter files by extension
+            };
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                ZipArchive zipArchive = ZipFile.OpenRead(filename);
+                IList<ZipArchiveEntry> entries = zipArchive.Entries.ToList();
+
+                mRoot = new FileTreeNode(null);
+                var sorted = entries.OrderBy(entry => entry.FullName);
+                foreach (ZipArchiveEntry entry in entries)
+                {
+                    mRoot.Insert(entry);
+                }
+
+                MountDokan();
+            }
+        }
+
+        private void MountDokan()
+        {
+            Thread thread1 = new Thread(this.MountWorker);
+            thread1.Start();
+        }
+
+        private void MountWorker()
+        {
+            var dk = new ZipDokanOperation(mRoot);
+            dk.Mount(MOUNT_POINT + ":\\", DokanOptions.DebugMode | DokanOptions.StderrOutput);
         }
     }
 }
---
+++ ZipFileSystem/ZipTree/FileTreeNode.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Text;
+
+namespace ZipFileSystem.ZipTree
+{
+    class FileTreeNode
+    {
+        public ZipArchiveEntry mNodeEntry;
+        public List<FileTreeNode> mChildren;
+        public FileTreeNode mParent;
+
+        public FileTreeNode(ZipArchiveEntry entry)
+        {
+            mNodeEntry = entry;
+            if (IsDirectory())
+            {
+                mChildren = new List<FileTreeNode>();
+            }
+        }
+
+        public FileTreeNode FindByName(string name)
+        {
+            if (mNodeEntry != null && FullName() == name)
+            {
+                return this;
+            }
+            else if (!IsDirectory())
+            {
+                return null;
+            }
+            else
+            {
+                foreach (FileTreeNode child in mChildren)
+                {
+                    if (name.StartsWith(child.FullName()))
+                    {
+                        return child.FindByName(name);
+                    }
+                }
+                return null;
+            }
+        }
+
+        public FileTreeNode FindByEntry(ZipArchiveEntry entry)
+        {
+            if (mNodeEntry == entry)
+            {
+                return this;
+            }
+            else
+            {
+                foreach (FileTreeNode child in mChildren)
+                {
+                    var finded = child.FindByEntry(entry);
+                    if (finded != null)
+                    {
+                        return finded;
+                    }
+                }
+                return null;
+            }
+        }
+
+        public bool IsDirectory()
+        {
+            if (mNodeEntry == null)
+            {
+                return true;
+            }
+            return mNodeEntry.FullName.EndsWith("/");
+        }
+
+        private string FullName()
+        {
+            if (mNodeEntry == null)
+            {
+                return "";
+            }
+            return mNodeEntry.FullName;
+        }
+
+        public bool Insert(ZipArchiveEntry entry)
+        {
+            if (IsDirectory() && entry.FullName.StartsWith(FullName()))
+            {
+                foreach (FileTreeNode child in mChildren)
+                {
+                    if (child.Insert(entry))
+                    {
+                        return true;
+                    }
+                }
+                var node = new FileTreeNode(entry);
+                mChildren.Add(node);
+                node.mParent = this;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
